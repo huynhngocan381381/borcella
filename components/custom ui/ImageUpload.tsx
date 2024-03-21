9e3ff8001ae7e23d404da79f87d393010fffb302
Huynh Ngoc An <huynhngocan381381@gmail.com>
2024-03-21 22:24:54 +0700

    Create Collections

---
+++ components/custom ui/ImageUpload.tsx
@@ -0,0 +1,47 @@
+import { Plus, Trash } from 'lucide-react'
+import { CldUploadWidget, CloudinaryUploadWidgetInfo, CloudinaryUploadWidgetResults } from 'next-cloudinary'
+
+import Image from 'next/image'
+import { Button } from '../ui/button'
+
+interface ImageUploadProps {
+  value: string[]
+  onChange: (value: string) => void
+  onRemove: () => void
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, onRemove, value }) => {
+  const onSuccess = (result: CloudinaryUploadWidgetResults) => {
+    onChange((result.info as CloudinaryUploadWidgetInfo).secure_url)
+  }
+
+  return (
+    <div>
+      <div className="mb-4 flex flex-wrap items-center gap-4">
+        {value.map((url) => (
+          <div key={url} className="relative w-[200px] h-[200px]">
+            <div className="absolute top-0 right-0 z-10">
+              <Button type="button" onClick={onRemove} size="sm" className="bg-red-1 text-white">
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image src={url} alt="collection" className="object-cover rounded-lg" fill />
+          </div>
+        ))}
+      </div>
+
+      <CldUploadWidget uploadPreset="mqb2xfqd" onSuccess={onSuccess}>
+        {({ open }) => {
+          return (
+            <Button type="button" onClick={() => open()} className="bg-grey-1 text-white">
+              <Plus className="h-4 w-4 mr-2" />
+              Upload Image
+            </Button>
+          )
+        }}
+      </CldUploadWidget>
+    </div>
+  )
+}
+
+export default ImageUpload
